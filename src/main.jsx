6641918413312d9036544ec07f089598cae31113
Analytics Dashboard <developer@analytics-dashboard.local>
2026-04-18 09:11:51 +0600

    feat: integrate React Router and set up initial layout
    - Added @tailwindcss/vite and react-router as dependencies.
    - Refactored App component to use React Router with a RootLayout and Home component.
    - Created new components: AllProducts, Home, Navbar, and RootLayout.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
-import App from './App.jsx'
+import RootLayout from './layouts/RootLayout';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home/> },
+    ]
+  },
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+ <RouterProvider router={router} />,
   </StrictMode>,
 )
